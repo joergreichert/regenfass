3cb9f4b0ebf0c693fbe207a240d52ce13652e1c6
DevMiner <devminer@devminer.xyz>
2023-10-17 21:25:42 +0200

    it compiles

--- src/loraFunctions.h
+++ src/loraFunctions.h
@@ -15,17 +15,17 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-// static const u1_t PROGMEM APPEUI[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+constexpr char const appEui[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 // void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
 
 // This should also be in little endian format, see above.
-// static const u1_t PROGMEM DEVEUI[8] = {0x7A, 0xCD, 0x05, 0x00, 0x00, 0x00, 0x99, 0x70};
+constexpr char const devEui[8] = {0x7A, 0xCD, 0x05, 0x00, 0x00, 0x00, 0x99, 0x70};
 // void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
-// static const u1_t PROGMEM APPKEY[16] = {0x3F, 0x5D, 0xB5, 0xEC, 0x00, 0xF3, 0x44, 0x00, 0x00, 0xB6, 0xF9, 0x00, 0xB8, 0xB9, 0xA7, 0x75};
+constexpr char const appKey[16] = {0x3F, 0x5D, 0xB5, 0xEC, 0x00, 0xF3, 0x44, 0x00, 0x00, 0xB6, 0xF9, 0x00, 0xB8, 0xB9, 0xA7, 0x75};
 // void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
 
 static uint8_t mydata[] = "Hello, world!";
@@ -57,14 +57,15 @@ static const lmic_pinmap myPinmap = {
     .rst = LORA_RESET_PIN,
     // .dio = {/*dio0*/ LORA_DIO0_PIN, /*dio1*/ LORA_DIO1_PIN, /*dio2*/ LORA_DIO2_PIN},
     // .dio = {/*dio0*/ LMIC_UNUSED_PIN, /*dio1*/ LORA_DIO1_PIN, /*dio2*/ LMIC_UNUSED_PIN},
-    .dio = {/*dio0*/ LMIC_UNUSED_PIN, /*dio1*/ LORA_DIO1_PIN},
+    // .dio = {/*dio0*/ LMIC_UNUSED_PIN, /*dio1*/ LORA_DIO1_PIN},
+    .dio = {LORA_DIO1_PIN},
     // .rxtx_rx_active = LMIC_UNUSED_PIN,
     // .rssi_cal = 10,
     // .spi_freq = 8000000, // 8MHz
     // .pConfig = &myHalConfig};
 };
 
-RadioSx1262 radio(myPinmap, ImageCalibrationBand::band_863_870);
+RadioSx1262 radio(myPinmap, ImageCalibrationBand::band_863_870, true);
 LmicEu868 LMIC{radio};
 
 void printHex2(unsigned v)
@@ -78,11 +79,16 @@ void printHex2(unsigned v)
 void loraSetup()
 {
     log_d("Setting up lora...");
-    // LMIC init
-    os_init();
 
+    SPI.begin(LORA_SCK_PIN, LORA_MISO_PIN, LORA_MOSI_PIN, LORA_NSS_PIN);
+
+    os_init();
+    LMIC.init();
     // Reset the MAC state. Session and pending data transfers will be discarded.
     LMIC.reset();
+
+    SetupLmicKey<appEui, devEui, appKey>::setup(LMIC);
+    LMIC.setClockError(MAX_CLOCK_ERROR * 1 / 100);
 
     // Disable link check validation
     // Disable link-check mode and ADR, because ADR tends to complicate testing.
