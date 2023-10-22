a7c2afb033154164b4aef266f9d02fab3101dedd
Stefan Brand <seiichiro@seiichiro0185.org>
2023-10-22 18:12:16 +0200

    It's Alive!

--- src/loraFunctions.h
+++ src/loraFunctions.h
@@ -15,57 +15,45 @@
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-constexpr char const appEui[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+//constexpr char const appEui[8] = {0x42, 0x3A, 0x92, 0xA6, 0xF5, 0xBB, 0xBC, 0x18};
 // void os_getArtEui(u1_t *buf) { memcpy_P(buf, APPEUI, 8); }
 
 // This should also be in little endian format, see above.
-constexpr char const devEui[8] = {0x7A, 0xCD, 0x05, 0x00, 0x00, 0x00, 0x99, 0x70};
+//constexpr char const devEui[8] = {0x3A, 0xE1, 0x9C, 0xCB, 0x7D, 0x60, 0xF0, 0xCF};
 // void os_getDevEui(u1_t *buf) { memcpy_P(buf, DEVEUI, 8); }
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
-constexpr char const appKey[16] = {0x3F, 0x5D, 0xB5, 0xEC, 0x00, 0xF3, 0x44, 0x00, 0x00, 0xB6, 0xF9, 0x00, 0xB8, 0xB9, 0xA7, 0x75};
+//constexpr char const appKey[16] = {0xA3, 0x46, 0xE1, 0xB1, 0x2B, 0x0A, 0x15, 0xD1, 0x43, 0xA6, 0x7D, 0x37, 0xE2, 0x8C, 0xEC, 0xE5};
 // void os_getDevKey(u1_t *buf) { memcpy_P(buf, APPKEY, 16); }
+
+
+// Die LMICPP-Arduino braucht den Kram als Strings!
+constexpr char const appEui[] = "ABCABCABCABCABCA";
+// Device EUI in string format.
+constexpr char const devEui[] = "DEFDEFDEFDEFDEFD";
+// Application key in string format.
+// constexpr char const appKey[] = "A346E1B12B0A15D143A67D37E28CECE5";
+constexpr char const appKey[] = "FFEEFFEEFFEEFFEEFFEEFFEEFFEEFFEE";
 
 static uint8_t mydata[] = "Hello, world!";
 uint8_t payload[34];
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 10;
+const unsigned TX_INTERVAL = 30;
 
 char TTN_response[30];
 
-// class MyHalConfig_t : public Arduino_LMIC::HalConfiguration_t
-// {
-
-// public:
-//     MyHalConfig_t(){};
-
-//     // set SPI pins to board configuration, pins may come from pins_arduino.h
-//     virtual void begin(void) override
-//     {
-//         SPI.begin(LORA_SCK_PIN, LORA_MISO_PIN, LORA_MOSI_PIN, LORA_NSS_PIN);
-//     }
-// };
-
-// static MyHalConfig_t myHalConfig{};
 static const lmic_pinmap myPinmap = {
     .nss = LORA_NSS_PIN,
     .prepare_antenna_tx = nullptr,
     .rst = LORA_RESET_PIN,
-    // .dio = {/*dio0*/ LORA_DIO0_PIN, /*dio1*/ LORA_DIO1_PIN, /*dio2*/ LORA_DIO2_PIN},
-    // .dio = {/*dio0*/ LMIC_UNUSED_PIN, /*dio1*/ LORA_DIO1_PIN, /*dio2*/ LMIC_UNUSED_PIN},
-    // .dio = {/*dio0*/ LMIC_UNUSED_PIN, /*dio1*/ LORA_DIO1_PIN},
-    .dio = {LORA_DIO1_PIN},
-    // .rxtx_rx_active = LMIC_UNUSED_PIN,
-    // .rssi_cal = 10,
-    // .spi_freq = 8000000, // 8MHz
-    // .pConfig = &myHalConfig};
+    .dio = {LORA_DIO0_PIN, LORA_DIO1_PIN},
 };
 
-RadioSx1262 radio(myPinmap, ImageCalibrationBand::band_863_870, true);
+RadioSx1262 radio(myPinmap, ImageCalibrationBand::band_863_870);
 LmicEu868 LMIC{radio};
 
 void printHex2(unsigned v)
